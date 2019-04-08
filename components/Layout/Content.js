a259570e27689a2342a564009ac154200c5464a1
Tien Pham <r3dsky02@yahoo.com.vn>
2019-04-08 22:27:49 +0300

    Content

---
+++ components/Layout/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { node, string, oneOfType } from "prop-types";
+import { Layout } from "antd";
+
+const AppContent = Layout.Content;
+const Content = ({ children, ...props }) => (
+  <AppContent {...props}>{children}</AppContent>
+);
+Content.propTypes = {
+  children: oneOfType([node, string])
+};
+export default Content;
